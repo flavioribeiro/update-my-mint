8d84ca5bbfa7ecf1ef6e982ff68ddc2bde97ec6e
NeonWizard <averagewizard13@gmail.com>
2018-06-18 15:36:33 -0600

    Python3 compatibility

--- usage.py
+++ usage.py
@@ -12,7 +12,7 @@ def get_coinspring(login, passwd):
 
 if __name__ == '__main__':
     mint_login = 'login_here'
-    mint_passwd= 'passwd_here'
+    mint_passwd = 'passwd_here'
 
     my_accounts = {
         'Robinhood': {'callback': get_robinhood, 'login': 'login_here', 'password': 'password_here'},
@@ -23,10 +23,10 @@ if __name__ == '__main__':
 
     for account in mint.get_accounts():
         if my_accounts.has_key(account['name']):
-            print 'found', account['name']
+            print('found', account['name'])
             acc = my_accounts[account['name']]
             new_value = acc['callback'](acc['login'], acc['password'])
-            print 'updating', account['name'], 'with US$', new_value
+            print('updating', account['name'], 'with US$', new_value)
             mint.set_property_account_value(account, new_value)
 
     mint.driver.close()
